5fa6304332caf0220fcb2600254f2fbba31631c6
slfiene <sean.fiene@nintex.com>
2017-06-20 09:20:15 -0700

    Update Index.js again

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const raspi = require('raspi-io');
-const five = require('johnny-five');
-const board = new five.Board({
-  io: new raspi()
+var five = require("johnny-five");
+var Raspi = require("raspi-io");
+var board = new five.Board({
+  io: new Raspi()
 });
 
-board.on('ready', () => {
-  // Create an Led on pin 7 on P1 (GPIO4)
-  // and strobe it on/off
-  const led = new five.Led('P1-7');
-  led.strobe(500);
+board.on("ready", function() {
+  var led = new five.Led("P1-13");
+  led.blink();
 });
